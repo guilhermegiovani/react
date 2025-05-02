2a5fa3e813cbdfd63dccda2c47ed8991261c2b91
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-05-02 15:33:11 -0300

    app-movies

---
+++ react_edsonMaia/app-movies/src/routes.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import Search from "./pages/Search";
+import Watch from "./pages/Watch";
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <Home /> } ></Route>
+                <Route path="/watch/:id" element={ <Watch /> } ></Route>
+                <Route path="/search" element={ <Search /> } ></Route>
+                <Route path="*" element={ <PageNotFound /> } ></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRoutes;

79e7de6aaffae01e25ccba9788f6d1a19dd8b196
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-05-27 11:12:22 -0300

    Iniciando Gerenciado de Finanças

---
+++ projetosPraticos/gerenciador_financas/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <section>
+      <h1>Gerenciador de Finanças</h1>
+    </section>
+      
+  )
+}
+
+export default App
